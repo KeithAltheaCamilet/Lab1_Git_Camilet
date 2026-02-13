8ebbc018b1d4e8bc2e98d90ec65d804da4ab2264
KeithAltheaCamilet <keithaltheaaa@gmail.com>
2026-02-13 16:29:43 +0800

    Added section info for Camilet.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Lab1: Version Control")
-print("Author:[KEITH ALTHEA R. CAMILET]")
+print("Author: KEITH ALTHEA R. CAMILET")
 print("Student ID: TUPM-25-0614")
+print("Class Section: BSME 1B")
